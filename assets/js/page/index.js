d07fa33a8842861af3f21b90bfdffb02a5f416ad
FXQK <79103187@qq.com>
2016-09-29 16:49:51 +0800

    修改日程页样式，独立添加按钮样式

--- assets/js/page/index.js
+++ assets/js/page/index.js
@@ -12,28 +12,6 @@ var fuc = {
         this.changeBg();
         this.bindEvent();
     },
-    pageLoad: function(options) {
-        document.addEventListener('touchmove', function (e) {//禁止浏览器上下滑动页面
-            e.preventDefault();
-        });
-
-        var defaults = {opacity: 1, backgroundColor: "#000", delayTime: 500, zindex: 999, sleep: 500};
-        var options = $.extend(defaults, options);
-        var _PageHeight = document.documentElement.clientHeight, _PageWidth = document.documentElement.clientWidth;
-        var _LLLoadingHtml = '<div id="loadingPage" style="position:fixed;left:0;top:0;_position: absolute;width:100%;height:' + _PageHeight + 'px;background:' + options.backgroundColor + ';opacity:' + options.opacity + ';filter:alpha(opacity=' + options.opacity * 100 + ');z-index:' + options.zindex + ';"><div class="ll_loading_con"><div class="ll-loading"><div class="ll-load-inner"><div class="ll-load-container"><div class="ll-load-scale-multiple la-2x"><div></div><div></div><div></div></div></div></div><div class="ll-load-logo"><span class="ll-logo-1"></span><span class="ll-logo-2"></span><span class="ll-logo-3"></span></div></div></div></div>';
-        $("body").append(_LLLoadingHtml);
-        document.onreadystatechange = PageLoaded;
-        function PageLoaded() {
-            if (document.readyState == "complete") {
-                var loadingMask = $('#loadingPage');
-                setTimeout(function () {
-                    loadingMask.animate({"opacity": 0}, options.delayTime, function () {
-                        $(this).remove()
-                    })
-                }, options.sleep)
-            }
-        }
-    },
     renderPage: function() {
         wx.wxConfig(1);
         var that = this;
